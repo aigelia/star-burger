ffcba8546fca9ab461f4cdb4826353bac96a4041
Katerina <e.mayakovskaya@gmail.com>
2025-09-08 09:13:48 +0300

    Fix migration

--- geolocations/migrations/0004_recreate_location_table.py
+++ geolocations/migrations/0004_recreate_location_table.py
@@ -6,15 +6,4 @@ class Migration(migrations.Migration):
         ('geolocations', '0003_remove_location_foodcartapp_lat_cc2ff7_idx_and_more'),
     ]
 
-    operations = [
-        migrations.RunSQL(
-            """
-            CREATE TABLE geolocations_location (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                lng NUMERIC(9,6) NOT NULL,
-                lat NUMERIC(9,6) NOT NULL,
-                address VARCHAR(255) UNIQUE NULL
-            );
-            """
-        ),
-    ]
+    operations = []
